d9065210db82d5a9e7e1600b9bc4af1ed2f21bb6
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-09-16 19:00:13 +0100

    Working up to twenty

--- numberToWords.js
+++ numberToWords.js
@@ -1,5 +1,49 @@
-const numberToWords = () => {
-  return 'five';
+const numberToWords = (source) => {
+  switch (source) {
+    case 0:
+      return 'zero';
+    case 1:
+      return 'one';
+    case 2:
+      return 'two';
+    case 3:
+      return 'three';
+    case 4:
+      return 'four';
+    case 5:
+      return 'five';
+    case 6:
+      return 'six';
+    case 7:
+      return 'seven';
+    case 8:
+      return 'eight';
+    case 9:
+      return 'nine';
+    case 10:
+      return 'ten';
+    case 11:
+      return 'eleven';
+    case 12:
+      return 'twelve';
+    case 13:
+      return 'thirteen';
+    case 14:
+      return 'fourteen';
+    case 15:
+      return 'fifteen';
+    case 16:
+      return 'sixteen';
+    case 17:
+      return 'seventeen';
+    case 18:
+      return 'eighteen';
+    case 19:
+      return 'nineteen';
+    case 20:
+      return 'twenty';
+  }
+  return 'TODO';
 }
 
 
